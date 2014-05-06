52f150229699893a9b68af0977bbb6a6ed4e6c00
Artur Czeczko <a.czeczko@icm.edu.pl>
2014-05-06 11:44:52 +0200

    Implemented logs count ignoring multiple access in the same session

---
+++ synatlogsanalysis-impl/src/main/java/pl/edu/icm/synatlogsanalysis/SpecialCountSummary.java
@@ -0,0 +1,54 @@
+package pl.edu.icm.synatlogsanalysis;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import java.util.HashSet;
+import java.util.Set;
+import org.apache.hadoop.io.BytesWritable;
+import pl.edu.icm.coansys.models.StatisticsProtos;
+import pl.edu.icm.coansys.statisticsgenerator.operationcomponents.StatisticCalculator;
+
+/**
+ *
+ * @author Artur Czeczko <a.czeczko@icm.edu.pl>
+ */
+public class SpecialCountSummary implements StatisticCalculator {
+    
+    private static final String sessionIdLabel = "sessionId";
+
+    public double calculate(Iterable<BytesWritable> messages) {
+        double result = 0.0;
+        
+        Set<String> sessions = new HashSet<String>();
+        
+        for (BytesWritable bw : messages) {
+            try {
+                StatisticsProtos.InputEntry inputEntry = StatisticsProtos.InputEntry.parseFrom(bw.copyBytes());
+                String sessionId = getFieldValue(inputEntry, sessionIdLabel);
+                if (!sessionId.isEmpty()) {
+                    if (!sessions.contains(sessionId)) {
+                        result += 1.0;
+                        sessions.add(sessionId);
+                    }
+                } else {
+                    result += 1.0;
+                }
+            } catch (InvalidProtocolBufferException ex) {
+                throw new IllegalArgumentException(ex);
+            }
+        }
+        
+        return result;
+    }
+
+    public void setup(String... params) {
+    }
+    
+    private String getFieldValue(StatisticsProtos.InputEntry entry, String fieldName) {
+        for (StatisticsProtos.KeyValue kv : entry.getFieldList()) {
+            if (fieldName.equals(kv.getKey())) {
+                return kv.getValue();
+            }
+        }
+        return "";
+    }
+}
